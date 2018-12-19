17695a667b58f83a31ac4fdc65a0968cf4da83a9
Christine Loong <34012305+Ceredin@users.noreply.github.com>
2018-12-19 16:49:16 +1300

    Add files via upload

--- user_upload.php
+++ user_upload.php
@@ -1,16 +1,44 @@
 <?php
 
-if (($myfile = fopen("users.csv", "r")) !== FALSE) {
-    //a less dramatic way of handling the error than die()
+$shortopts ="uph";
+
+$longopts = array(
+    "file:",
+    "create_table",
+    "dry_run",
+    "help",
+);
+$options = getopt($shortopts, $longopts);
+
+//do some stuff
+
+var_dump($options);
 
 
-    
 
-    fclose($myfile);
-}else {
-    print ("File unable to be opened.");
+function openFile($file){
+    //when input is --file followed by the argument
+        if (($myfile = fopen($file, "r")) !== FALSE) {
+            
+
+
+
+
+            fclose($myfile);
+        }else {
+            print ("File unable to be opened.");
+        }
+
 }
 
+function create(){
+    //when --create_table is inputted
+
+}
+
+function dryRun(){
+    //when --dry_run is inputted
+}
 
 
 ?>
