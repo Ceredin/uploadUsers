ff50d6c69e466fedab68e8a99ba450fac2f51455
Christine Loong <34012305+Ceredin@users.noreply.github.com>
2018-12-19 17:28:09 +1300

    Add files via upload

--- user_upload.php
+++ user_upload.php
@@ -1,6 +1,6 @@
 <?php
 
-$shortopts ="uph";
+$shortopts ="u:p:h:";
 
 $longopts = array(
     "file:",
@@ -10,9 +10,34 @@ $longopts = array(
 );
 $options = getopt($shortopts, $longopts);
 
-//do some stuff
+//figure out what the directive was
+if($options["u"] != false){
+    //what comes next is the MySQL username
+    $username = $options["u"];
+}
+elseif($options["p"] != false){
+    //what comes next is the MySQL password
+    $password = $options["p"];
+}
+elseif($options["h"] != false){
+    //what comes next is the MySQL host
+    $host = $options["h"];
+}
+elseif($options["file"] != false){
+    $file = $options[file];
+    openFile($file);
+}
+elseif($options["create_table"] != false){
+    create();
+}
+elseif($options["dry_run"] != false){
+    dryRun();
+}
+elseif($options["help"] != false){
+    help();
+}
 
-var_dump($options);
+
 
 
 
@@ -40,5 +65,29 @@ function dryRun(){
     //when --dry_run is inputted
 }
 
+function help(){
+    //when --help is inputted
+    print("Commands \n
+    --file [csv file name] \n
+    Gives the name of the file to be parsed \n
+    \n 
+    --create_table\n
+    This will cause the MySQL users table to be built\n 
+    \n
+    --dry_run\n
+    used with the --file directive in the instance that we want to run the
+    script but not insert into the DB. All other functions will be executed, but the database won't
+    be altered.\n
+    \n
+    -u \nMySQL username\n
+    \n
+    -p \nMySQL password\n
+    \n
+    -h \nMySQL host\n
+    \n
+    --help\n
+    This is what you are seeing.\n
+    ");
+}
 
 ?>
