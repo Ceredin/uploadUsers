feaebb8169658ef28587cda053031c09738b366c
Christine Loong <34012305+Ceredin@users.noreply.github.com>
2018-12-19 19:04:01 +1300

    Add files via upload

--- user_upload.php
+++ user_upload.php
@@ -1,7 +1,6 @@
 <?php
 
 $shortopts ="u:p:h:";
-
 $longopts = array(
     "file:",
     "create_table",
@@ -10,30 +9,36 @@ $longopts = array(
 );
 $options = getopt($shortopts, $longopts);
 
-//figure out what the directive was
-if($options["u"] != false){
+//figure out what the directive was... 
+if(array_key_exists("u", $options)){
     //what comes next is the MySQL username
     $username = $options["u"];
+
 }
-elseif($options["p"] != false){
+if(array_key_exists("p", $options)){
     //what comes next is the MySQL password
     $password = $options["p"];
+ 
 }
-elseif($options["h"] != false){
+if(array_key_exists("h", $options)){
     //what comes next is the MySQL host
     $host = $options["h"];
 }
-elseif($options["file"] != false){
-    $file = $options[file];
-    openFile($file);
+if(array_key_exists("file", $options)){
+    $file = $options["file"];
 }
-elseif($options["create_table"] != false){
-    create();
+if(array_key_exists("create_table", $options)){
+    if($file != false){
+        create();
+    }
 }
-elseif($options["dry_run"] != false){
-    dryRun();
+if(array_key_exists("dry_run", $options)){
+    if($file != false){
+        dryRun();
+    }
 }
-elseif($options["help"] != false){
+
+if(array_key_exists("help", $options)){
     help();
 }
 
@@ -41,14 +46,11 @@ elseif($options["help"] != false){
 
 
 
+
 function openFile($file){
     //when input is --file followed by the argument
-        if (($myfile = fopen($file, "r")) !== FALSE) {
+        if (($myfile = fopen($file, "r")) !== false) {
             
-
-
-
-
             fclose($myfile);
         }else {
             print ("File unable to be opened.");
@@ -56,13 +58,13 @@ function openFile($file){
 
 }
 
+function dryRun(){
+    //when --dry_run is inputted
+}
+
 function create(){
     //when --create_table is inputted
 
-}
-
-function dryRun(){
-    //when --dry_run is inputted
 }
 
 function help(){
@@ -75,7 +77,7 @@ function help(){
     This will cause the MySQL users table to be built\n 
     \n
     --dry_run\n
-    used with the --file directive in the instance that we want to run the
+    Used with the --file directive in the instance that we want to run the
     script but not insert into the DB. All other functions will be executed, but the database won't
     be altered.\n
     \n
